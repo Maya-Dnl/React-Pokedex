2e308db21a87ddb3429c63e5b386551db6cbb3c1
Maya-Dnl <maya.dnl29@gmail.com>
2023-11-12 16:22:28 +0100

    refacto funcions date & type + create page pokemon details

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,6 +1,9 @@
 import React, { FunctionComponent, useState } from 'react';
 import Pokemon from '../models/pokemon';
 import './pokemon-card.css';
+import formatType from '../helpers/format-type';
+import formatDate from '../helpers/format-date';
+
   
 type Props = {
   pokemon: Pokemon,
@@ -20,56 +23,7 @@ const PokemonCard: FunctionComponent<Props> = ({pokemon, colorCard = "#26a69a"})
   const mouseLeaveColorCard = () => {
     setColor("#80cbc4");
   }
-
-  const formatDate = (date: Date): string => {
-    return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
-  }
-
-  const formatType = (type: string): string => {
-    let color: string;
-   
-    switch (type) {
-      case 'Feu': 
-        color = 'red lighten-1'; 
-        break; 
-      case 'Eau': 
-        color = 'blue lighten-1'; 
-        break; 
-      case 'Plante': 
-        color = 'green lighten-1'; 
-        break; 
-      case 'Insecte': 
-        color = 'brown lighten-1'; 
-        break; 
-      case 'Normal': 
-        color = 'grey lighten-3'; 
-        break; 
-      case 'Vol': 
-        color = 'blue lighten-3'; 
-        break; 
-      case 'Poison': 
-        color = 'deep-purple accent-1'; 
-        break; 
-      case 'Fée': 
-        color = 'pink lighten-4'; 
-        break; 
-      case 'Psy': 
-        color = 'deep-purple darken-2'; 
-        break; 
-      case 'Electrik': 
-        color = 'lime accent-1'; 
-        break; 
-      case 'Combat': 
-        color = 'deep-orange'; 
-        break; 
-      default: 
-        color = 'grey'; 
-        break; 
-    }
-   
-    return `chip ${color}`;
-  }
-
+  
   return (
     <div className="col s6 m4" onMouseEnter={mouseEnterColorCard} onMouseLeave={mouseLeaveColorCard}>
       <div className="card horizontal" style={{ backgroundColor: color }}>
