40bceb545037b66978053a64eb867b75ba8c9775
Maya-Dnl <maya.dnl29@gmail.com>
2023-11-22 15:22:14 +0100

    update pokemon-form file -> creation of the expected behaviour of the edit form (handle state init, state inputs user, redirection, submission)

--- src/components/pokemon-form.tsx
+++ src/components/pokemon-form.tsx
@@ -1,20 +1,78 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Pokemon from '../models/pokemon';
 import formatType from '../helpers/format-type';
   
 type Props = {
   pokemon: Pokemon
 };
+
+type Field = {
+  value?: any,
+  error?: string,
+  isValid?: boolean
+};
+
+type Form = {
+  name: Field,
+  hp: Field,
+  cp: Field,
+  types: Field
+}
   
 const PokemonForm: FunctionComponent<Props> = ({pokemon}) => {
+
+  const [form, setForm] = useState<Form>({
+    name: { value: pokemon.name, isValid: true },
+    hp: { value: pokemon.hp, isValid: true },
+    cp: { value: pokemon.cp, isValid: true },
+    types: { value: pokemon.types, isValid: true }
+  });
+
+  const history = useHistory();
   
   const types: string[] = [
     'Plante', 'Feu', 'Eau', 'Insecte', 'Normal', 'Electrik',
     'Poison', 'Fée', 'Vol', 'Combat', 'Psy'
   ];
+
+  const correspondingTypesPokemon = (type: string):boolean => {
+    return form.types.value.includes(type);
+  };
+
+  const selectType = (type: string, e: React.ChangeEvent<HTMLInputElement>): void => {
+    const checked = e.target.checked;
+    let newField: Field;
+
+    if(checked) {
+      // Si l'utilisateur coche un type, on l'ajoute à la liste des types du pokémon.
+      const newTypes: string[] = form.types.value.concat([type]);
+      newField = { value: newTypes };
+    } else {
+      // Si l'utilisateur décoche un type, on le retire de la liste des types du pokémon.
+      const newTypes: string[] = form.types.value.filter((currentType: string) => currentType !== type);
+      newField = { value: newTypes };
+    }
+
+    setForm({...form, ...{ types: newField }});
+  }
+
+  const handleChangeInputsValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const fieldName: string = event.target.name;
+    const fieldValue: string = event.target.value;
+    const newField: Field = {[fieldName]: {value: fieldValue}};
+
+    setForm({...form, ...newField});
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(form);
+    history.push(`/pokemon-list/${pokemon.id}`)
+  };
    
   return (
-    <form>
+    <form onSubmit={event => handleSubmit(event)}>  
       <div className="row">
         <div className="col s12 m8 offset-m2">
           <div className="card hoverable"> 
@@ -26,17 +84,17 @@ const PokemonForm: FunctionComponent<Props> = ({pokemon}) => {
                 {/* Pokemon name */}
                 <div className="form-group">
                   <label htmlFor="name">Nom</label>
-                  <input id="name" type="text" className="form-control"></input>
+                  <input id="name" name="name" type="text" className="form-control" value={form.name.value} onChange={event => handleChangeInputsValue(event)}></input>
                 </div>
                 {/* Pokemon hp */}
                 <div className="form-group">
                   <label htmlFor="hp">Point de vie</label>
-                  <input id="hp" type="number" className="form-control"></input>
+                  <input id="hp" name="hp" type="number" className="form-control" value={form.hp.value} onChange={event => handleChangeInputsValue(event)}></input>
                 </div>
                 {/* Pokemon cp */}
                 <div className="form-group">
                   <label htmlFor="cp">Dégâts</label>
-                  <input id="cp" type="number" className="form-control"></input>
+                  <input id="cp" name="cp" type="number" className="form-control" value={form.cp.value} onChange={event => handleChangeInputsValue(event)}></input>
                 </div>
                 {/* Pokemon types */}
                 <div className="form-group">
@@ -44,7 +102,7 @@ const PokemonForm: FunctionComponent<Props> = ({pokemon}) => {
                   {types.map(type => (
                     <div key={type} style={{marginBottom: '10px'}}>
                       <label>
-                        <input id={type} type="checkbox" className="filled-in"></input>
+                        <input id={type} type="checkbox" className="filled-in" value={type} checked={correspondingTypesPokemon(type)} onChange={ e => selectType(type, e)}></input>
                         <span>
                           <p className={formatType(type)}>{ type }</p>
                         </span>
