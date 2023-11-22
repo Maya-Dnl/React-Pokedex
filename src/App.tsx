155c6e02bca9a30d76706c864386203582d9fe5e
Maya-Dnl <maya.dnl29@gmail.com>
2023-11-22 10:56:37 +0100

    create page 404 & add route + import in app file

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import PokemonList from './pages/pokemon-list';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PokemonsDetail from './pages/pokemon-details';
 import NavBar from './components/nav-bar';
+import Page404 from './pages/page-404';
 
 const App: FunctionComponent = () => {
   
@@ -14,6 +15,7 @@ const App: FunctionComponent = () => {
           <Route exact path="/" component={PokemonList} />
           <Route exact path="/pokemon-list" component={PokemonList} />
           <Route path="/pokemon-list/:id" component={PokemonsDetail} />
+          <Route component={Page404} />
         </Switch>
       </div>
     </Router>
